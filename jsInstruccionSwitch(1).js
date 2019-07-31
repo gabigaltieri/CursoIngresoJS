180cff82fc967cee5ee33d77e60d60d6206c344c
gabigaltieri <52494246+gabigaltieri@users.noreply.github.com>
2019-07-31 12:22:19 -0300

    switch 1-3

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+ switch(mesDelAño)
+ {
+     case "Enero": {alert ("que comiences bien el año!");break};
+     case "Marzo": {alert ("a calses!!");break};
+     case "Julio": {alert ("ya vienen las vacaciones!!!");break};
+     case "Diciembre": {alert ("felices fiestas");break}; 
+
+
+
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,24 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+switch (mesDelAño) {
+    case "Enero":{alert ("ya pasamos el frio ahora calor");break};
+    case "Febrero":{alert ("ya pasamos el frio ahora calor");break};
+    case "Marzo": {alert ("Falta para el invierno");break};
+    case "Abril": {alert ("Falta para el invierno");break};
+    case "Mayo": {alert ("Falta para el invierno");break};
+    case "junio": {alert ("Falta para el invierno");break};
+    case "Julio": {alert ("abrigate que hace frio");break};
+    case "Agosto": {alert ("abrigate que hace frio");break};
+    case "Septiembre": {alert ("ya pasamos el frio ahora calor");break};
+    case "Octubre": {alert ("ya pasamos el frio ahora calor");break};
+    case "Noviembre": {alert ("ya pasamos el frio ahora calor");break};
+    case "Diciembre":{alert("ya pasamos el frio ahora calor");break};
+}
+
+
+
+
+}//FIN DE LA FUNCIÓN
